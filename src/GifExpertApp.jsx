d2ee07e24479402a881034e219d4d57335e85b14
arnaldotorr <arnaldo.torrealba@gmail.com>
2024-05-04 18:28:56 -0400

    Create AddCategory component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,39 @@
+import { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+
+  const onAddCategory = newCategory => {
+    // Verificamos que no exista la nueva categoría
+    const lowerCaseNewCategory = newCategory.toLowerCase()
+    const lowerCaseCategories = categories.map(category =>
+      category.toLowerCase()
+    )
+    if (lowerCaseCategories.includes(lowerCaseNewCategory)) return
+
+    // Actualizamos las categorías
+    // setCategories([...categories, 'Valorant'])
+    setCategories(cat => [newCategory, ...cat])
+  }
+
   return (
     <>
       {/* Título */}
       <h1>GifExpertApp</h1>
 
       {/* Input */}
+      <AddCategory
+        // setCategories={setCategories}
+        onNewCategory={onAddCategory}
+      />
 
       {/* Listado de Gif */}
+      <ol>
+        {categories.map(category => {
+          return <li key={category}>{category}</li>
+        })}
+      </ol>
       {/* Gif Item */}
     </>
   )
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState('')
+
+  const onInputChange = event => {
+    setInputValue(event.target.value)
+  }
+
+  const onSubmit = event => {
+    event.preventDefault()
+    const newInput = inputValue.trim()
+
+    if (newInput.length <= 1) return
+    // setCategories(cat => [inputValue, ...cat])
+    onNewCategory(newInput)
+    setInputValue('')
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type='text'
+        placeholder='buscar gifs....'
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+  )
+}
