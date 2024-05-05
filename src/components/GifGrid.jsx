a7868d24abec574cc29f5c6dada8765fc7684bea
arnaldotorr <arnaldo.torrealba@gmail.com>
2024-05-04 23:02:57 -0400

    Show loading message

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -9,6 +9,8 @@ export const GifGrid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
+
       <div className='card-grid'>
         {images.map(img => (
           <GifItem key={img.id} {...img} />
